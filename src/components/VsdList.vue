898e6622ed824c360a3c87703391e92eaba51743
Andrew Tolstov <awordes76@gmail.com>
2021-03-08 13:31:14 +0300

    Добавлен список транзакций пользователя.

--- src/components/VsdList.vue
+++ src/components/VsdList.vue
@@ -26,8 +26,8 @@
                     </b-form-select>
                 </b-col>            
                 <b-col>
-                    <b-form-select v-model="pageSize">
-                        <b-form-select-option v-for="size in pageSizes" v-bind:value="size.value" v-bind:key="size.value">
+                    <b-form-select v-model="vsdListPageSize">
+                        <b-form-select-option v-for="size in vsdListPageSizes" v-bind:value="size.value" v-bind:key="size.value">
                             {{ size.text }}
                         </b-form-select-option>
                     </b-form-select>
@@ -44,7 +44,7 @@
                     <b-table selectable outlined hover responsive
                         :items="vsdList"
                         :fields="vsdFields"
-                        :busy="isLoading"
+                        :busy="vsdListIsLoading"
                         :select-mode="selectMode"
                         ref="vsdListTable"
                         @row-selected="onRowSelected">
@@ -66,13 +66,20 @@
             <b-row>
                 <b-col>
                     <b-pagination
-                        v-model="currentPage"
-                        :per-page="pageSize"
-                        :total-rows="rowCount"
+                        v-model="vsdListCurrentPage"
+                        :per-page="vsdListPageSize"
+                        :total-rows="vsdListRowCount"
                         @change="getVsdList"
                         align="left"
                         last-number>
                     </b-pagination>
+                </b-col>
+                <b-col>
+                    <b-button variant="success" class="submit" 
+                        v-on:click="processVsdList(selectedRows)" 
+                        :disabled="selectedRows.length < 1 || vsdProcessingInProgress">
+                        <b-spinner small :hidden="!vsdProcessingInProgress"></b-spinner>
+                        Погасить выбранные ВСД</b-button>
                 </b-col>
                 <b-col>
                     <b-button size="sm" variant="outline-info" @click="selectAll" style="margin: 5px;">Выбрать всё</b-button>
@@ -82,11 +89,34 @@
         </b-container>
         
         <b-container class="vsd-wr">
+            <b-row>
+                <b-col>
+                    <b-table class="vsd-log" outlined hover responsive
+                        :items="vsdProcessTransactionList"
+                        :fields="vsdProcessTransactionFields"
+                        ref="vsdProcessTransactionListTable">
+                        <template #cell(startTime)="data">
+                            <span>{{ data.item.startTime | moment("MM.DD.YYYY hh:mm:ss") }}</span>
+                        </template>
+                        <template #cell(finishTime)="data">
+                            <span>{{ data.item.finishTime | moment("MM.DD.YYYY hh:mm:ss") }}</span>
+                        </template>
+                        <template #cell(error)="data">
+                            <span class="nowrap">{{data.item.error}}</span>
+                        </template>
+                    </b-table>
+                </b-col>
+            </b-row>
             <b-row>                
                 <b-col>
-                    <b-button variant="success" class="submit" 
-                        v-on:click="processVsdList(selectedRows)" 
-                        :disabled="selectedRows.length < 1">Погасить выбранные ВСД</b-button>
+                    <b-pagination
+                        v-model="vsdProcessTransactionCurrentPage"
+                        :per-page="vsdProcessTransactionPageSize"
+                        :total-rows="vsdProcessTransactionRowCount"
+                        @change="getVsdProcessTransactionList"
+                        align="left"
+                        last-number>
+                    </b-pagination>
                 </b-col>
             </b-row>
         </b-container>
@@ -100,15 +130,15 @@ export default {
     name: "VsdList",
     data() {
         return {
-            isLoading: false,
+            vsdListIsLoading: false,
             selectMode: 'multi',
             vsdFields:[
                 {key: 'id', label: 'Идентификатор ВСД'},
                 {key: 'productDate', label: 'Дата выработки'},
                 {key: 'expirationDate', label: 'Дата окончания срока годности'},
                 {key: 'name', label: 'Наименование'},
-                {key: 'volume', label: 'Объем'},            
-                {key: 'productGlobalId', label: 'Номер продукции'},
+                {key: 'volume', label: 'Объем'},
+                {key: 'productGlobalId', label: 'Номер продукции'}
             ],
             vsdList: [],
             selectedRows:[],
@@ -118,7 +148,7 @@ export default {
             selectedStatus: null,
             enterprises: [],
             selectedEnterprise: null,
-            pageSizes: [
+            vsdListPageSizes: [
                 {
                     value: 10,
                     text: "Показывать по: 10"
@@ -133,33 +163,43 @@ export default {
                     text: "Показывать по: 100"
                 }
             ],
-            pageSize: 10,
-            currentPage: null,
-            pageCount: 0,
-            rowCount: 0,
-            perPage: 3
+            vsdListPageSize: 10,
+            vsdListCurrentPage: null,
+            vsdListRowCount: 0,
+            vsdProcessTransactionFields: [
+                {key: 'id', label: 'Идентификатор транзакции', thStyle: {width: '25%'}},
+                {key: 'startTime', label: 'Начало', thStyle: {width: '13%'}},
+                {key: 'finishTime', label: 'Завершение', thStyle: {width: '13%'}},
+                {key: 'vsdId', label: 'ВСД', thStyle: {width: '25%'}},
+                {key: 'error', label: 'Ошибка'}
+            ],
+            vsdProcessTransactionList: [],
+            vsdProcessingInProgress: false,            
+            vsdProcessTransactionPageSize: 10,
+            vsdProcessTransactionCurrentPage: null,
+            vsdProcessTransactionPageCount: 0,
+            vsdProcessTransactionRowCount: 0,
         }
     },
     methods: {
-        getVsdList: function(currentPage) {
-            this.isLoading = true;
+        getVsdList: function(vsdListCurrentPage) {
+            this.vsdListIsLoading = true;
             Vue.axios.get(this.$baseUrl + '/Vsd/GetVsdList', {
                 params: {
-                    "pageSize": this.pageSize,
-                    "page" : currentPage,
+                    "pageSize": this.vsdListPageSize,
+                    "page" : vsdListCurrentPage,
                     "status" : this.selectedStatus,
                     "type" : this.selectedType,
                     "enterpriseId": this.selectedEnterprise,
                 }
             })
             .then((response) => {
-                this.isLoading = false;
+                this.vsdListIsLoading = false;
                 this.vsdList = response.data.result;
-                this.pageCount = response.data.pageCount;
-                this.rowCount = response.data.elementCount;
+                this.vsdListRowCount = response.data.elementCount;
             }, (error) => {
                 console.log(error);
-                this.isLoading = false;
+                this.vsdListIsLoading = false;
                 this.$createNotification('danger', 'Ошибка на сервере', error.response.data.error);
             })
         },
@@ -175,24 +215,46 @@ export default {
         onRowSelected(items) {
             this.selectedRows = items.map(item => item.id);
         },
-
         processVsdList(uuids) {
-            this.$loaderStart();
+            this.interval = setInterval(() => this.getVsdProcessTransactionList(1), 1000);
+            this.vsdProcessingInProgress = true;
             Vue.axios.post(this.$baseUrl + '/Vsd/ProcessVsdList', {
                 "enterpriseId": this.selectedEnterprise,
                 "uuids": uuids
             })
             .then(() => {
-                this.$loaderEnd();
+                clearInterval(this.interval);
+                this.getVsdProcessTransactionList(1);
+                this.vsdProcessingInProgress = false;
+                this.vsdList = [];
+                this.vsdListRowCount = 0;
                 this.$createNotification('success', 'ВСД успешно погашены', ' ');
             }, (error) => {
                 console.log(error);
-                this.$loaderEnd();
+                clearInterval();
+                this.vsdProcessingInProgress = false;
                 this.$createNotification('danger', 'Ошибка на сервере', error.response.data.error);
             })
-        }
+        },        
+        getVsdProcessTransactionList(vsdProcessTransactionCurrentPage) {
+            Vue.axios.get(this.$baseUrl + '/VsdProcessTransaction/List', {
+                params: {
+                    "pageSize": this.vsdProcessTransactionPageSize,
+                    "page" : vsdProcessTransactionCurrentPage
+                }
+            })
+            .then((response) => {
+                this.vsdProcessTransactionList = response.data.results;
+                this.vsdProcessTransactionPageCount = response.data.pageCount;
+                this.vsdProcessTransactionRowCount = response.data.rowCount;
+            }, (error) => {
+                console.log(error);
+                this.$createNotification('danger', 'Ошибка на сервере', error.response.data.error);
+            })
+        },
     },
     mounted: function() {
+        this.getVsdProcessTransactionList(1);
         Vue.axios.get(this.$baseUrl + '/Vsd/GetVsdTypes')
         .then((response) => {
             this.types = response.data.enumElements;
@@ -238,5 +300,9 @@ export default {
     margin-top: 40px;
     margin-bottom: 20px;    
     padding: 15px;
+}
+
+.vsd-log {
+    font-size: 80%;
 }
 </style>
